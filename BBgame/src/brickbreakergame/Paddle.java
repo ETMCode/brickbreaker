c994a79a76f652ac0e7e3aa69271d86d656f2e30
ETMCode <ethantmartinez@gmail.com>
2026-04-21 02:00:25 -0400

    Update Paddle.java

--- BBgame/src/brickbreakergame/Paddle.java
+++ BBgame/src/brickbreakergame/Paddle.java
@@ -1,40 +1,47 @@
 //Ethan Martinez BrickBreaker Paddle(s) Portion
-//sorry for all the comments I overly comment and can delete if needed!
-//push at 8:00pm!!!!
+
 package brickbreakergame;
-//import java.util.Scanner;
-//removed scanner *4/14/26
 import javax.swing.Keystroke; 
 import java.awt.event.KeyEvent;
 //allows for VK(s) virtual keys
 import java.awt.event.KeyListener;
+
 //the guidlines for what happens with the keyboard (we can use it for bricks if we wanted/will talk in class) *powerups??!?*
 //I think keystroke is already imported could be wrong though
 //(w800, h600) <-Jframes size limits
+
 
 
 //Adding implements KeyListener will tell our main this is the section that ultilizes those inputs later in JFrame
 public class Paddle implements KeyListener {
 //This is one of two paddles and Paddle class may change to Paddle1 *4/13/26
 //Theres 2 ways to do this either if statements or utilizing the keystroke command which is tied to the Jframe
-// Scannner Keys1 = new Scanner(System.in); (was going to use this but requires ENTER after every input so no go)
 
-  private int x = **xx;
-  private int y = 700;
-//This is the Paddles1 starting point in JFrame (should be middle and almost at bottom)
+private int x;
+private int y;
+private int paddlelength;
+private int paddlewidth;
+//Decleration of the int(s)
+
+  
+  private int x = 400;
+  private int y = 100;
+  private int paddlelength =
+  private int paddlewidth = 
 
   public void keyPressed(KeyEvent Input1)
   {
     int button = Input1.getKeyCode();
  //VK stands for virtual key! Its much easier to call then getting the hexcode number and when I add a second paddle I can use VK to call the arrow keys as oppsed to finding their respective hexnumber 
- //Was going to use arrow keys at first but for now just want to test this all with A and D to make it easier (*can change ASAP if needed*)
+ 
 
 
   //add hold down button options
-  if (button == KeyEvent.VK_A) { x = x -1; }
- //moves paddle1 to the left (change -1 if we want to increase the space it moves)
-  if (button == KeyEvent.VK_D) { x = x +1; }
- //moves paddle1 to the right (change +1 if we want to increase the space it moves)
+  if (button == KeyEvent.VK_A) { x = x -10; }
+ //moves paddle1 to the left 
+    
+  if (button == KeyEvent.VK_D) { x = x +10; }
+ //moves paddle1 to the right
   }
     
   public void keyReleased(KeyEvent Input1) {}
