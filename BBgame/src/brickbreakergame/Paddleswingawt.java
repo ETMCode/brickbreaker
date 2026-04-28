d00c7f15861f9fff043ff8e00c6abacf976e3c69
ETMCode <ethantmartinez@gmail.com>
2026-04-28 18:34:38 -0400

    paddle update
    paddle change

--- BBgame/src/brickbreakergame/Paddleswingawt.java
+++ BBgame/src/brickbreakergame/Paddleswingawt.java
@@ -1,10 +1,23 @@
 package brickbreakergame;
 
+import java.awt.Color;
+import java.awt.Graphics;
+
+
+
+//this paddles graphic class all gui stuff should go here for paddle
 public class Paddleswingawt {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	{
+	
+public void draw(Graphics g) {
+			    g.setColor(Color.WHITE);
+			    g.fillRect(x, y, paddlewidth, paddlelength);
+//
+			    
+			    
+			   
+			    
 	}
 
 }
